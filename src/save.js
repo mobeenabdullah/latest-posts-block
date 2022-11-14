cd8ef6dae439467fc41e2e35bd34e9ed659f658b
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-11-15 03:25:24 +0500

    Displaying dynamic content in the front-end

--- src/save.js
+++ src/save.js
@@ -1,10 +1,3 @@
-import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
-
 export default function Save() {
-	return (
-		<p {...useBlockProps.save()}>
-			{__('Latest Posts – hello from the saved content!', 'latest-posts')}
-		</p>
-	);
+	return null;
 }
