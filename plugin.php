594da7d3cde01cada2b30374883b20dc57ba6ee5
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-11-15 03:34:37 +0500

    Displaying the latest posts in the front-end using PHP

--- plugin.php
+++ plugin.php
@@ -22,8 +22,36 @@
  */
 
 function blocks_course_render_latest_posts_block($attributes) {
-	var_dump($attributes);
-	return 'Dynamic Content';
+	$args = array(
+		'posts_per_page' => $attributes['numberOfPosts'],
+		'post_status' => 'publish'
+	);
+	$recent_posts = get_posts($args);
+
+	$posts = '<ul ' . get_block_wrapper_attributes() . '>';
+	foreach($recent_posts as $post) {
+		$title = get_the_title($post);
+		$title = $title ? $title : __('(No title)','latest-posts');
+		$permalink = get_permalink( $post );
+		$excerpt = get_the_excerpt( $post );
+
+		$posts .= '<li>';
+
+		if($attributes["displayFeaturedImage"] && has_post_thumbnail( $post )) {
+			$posts .= get_the_post_thumbnail( $post, 'large' );
+		}
+		$posts .= '<h5><a href="' . esc_url($permalink) . '">' . $title . '</a></h5>';
+		$posts .= '<time datetime="' . esc_attr( get_the_date('c', $post)) . '">' . esc_html( get_the_date('', $post)) . '</time>';
+
+		if(!empty($excerpt)) {
+			$posts .= '<p>' . $excerpt . '</p>';
+		}
+
+		$posts .= '</li>';
+	}
+	$posts .= '</ul>';
+
+	return $posts;
 }
 
 function blocks_course_latest_posts_block_init() {
