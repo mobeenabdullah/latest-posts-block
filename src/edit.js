499d5775031f4e9d3a374778954e4537f8288220
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-11-15 03:50:57 +0500

    Preparing our data for the editor loop

--- src/edit.js
+++ src/edit.js
@@ -1,11 +1,24 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
 import './editor.scss';
 
-export default function Edit() {
+export default function Edit({ attributes }) {
+	const { numberOfPosts } = attributes;
+	const posts = useSelect(
+		(select) => {
+			return select('core').getEntityRecords('postType', 'post', {
+				per_page: numberOfPosts,
+				_embed: true,
+			});
+		},
+		[numberOfPosts]
+	);
+	console.log(posts);
+
 	return (
 		<p {...useBlockProps()}>
-			{__('Latest Posts – hello from the editor!', 'latest-posts')}
+			{__('Boilerplate – hello from the editor!', 'boilerplate')}
 		</p>
 	);
 }
