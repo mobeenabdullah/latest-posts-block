afaf7f8c3bbd7dbb053686b421763ff774cc0f72
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-11-16 04:57:13 +0500

    Adding an option for changing the order of the posts

--- plugin.php
+++ plugin.php
@@ -24,7 +24,9 @@
 function blocks_course_render_latest_posts_block($attributes) {
 	$args = array(
 		'posts_per_page' => $attributes['numberOfPosts'],
-		'post_status' => 'publish'
+		'post_status' => 'publish',
+		'order' => $attributes['order'],
+		'orderby' => $attributes['orderBy'],
 	);
 	$recent_posts = get_posts($args);
 
